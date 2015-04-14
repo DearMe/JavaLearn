5276b3d4488919f4abaf449ded244e7e025c1780
DearMe <ruanqixiang@gmail.com>
2015-04-15 00:03:33 +0800

    add some classes

---
+++ JavaLearn/src/com/rqx/containers/Groundhog.java
@@ -0,0 +1,11 @@
+package com.rqx.containers;
+
+public class Groundhog {
+	protected int number;
+	public Groundhog(int n){
+		number = n;
+	}
+	public String toString(){
+		return "Groundhog #" + number;
+	}
+}
---
+++ JavaLearn/src/com/rqx/containers/SpringDetector.java
@@ -0,0 +1,13 @@
+package com.rqx.containers;
+
+public class SpringDetector {
+	public static <T extends Groundhog>
+	void detectSpring(Class<T> type) throws Exception{
+		 
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
